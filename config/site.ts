b133065e9b783fcb32e556ee55659749f40dd6af
Dev Talan <chaudhary.dev.talan@gmail.com>
2024-07-25 14:24:36 +0530

    perf: implemented good metadata for the application

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+import { SiteConfig } from '@/types';
+import { env } from '@/env.mjs';
+
+export const siteConfig: SiteConfig = {
+  name: 'Urban Rhetoric',
+  description:
+    'An open source MDX blog application built using Nextjs 14, server component, authjs, tailwind, and shadcn ui',
+  url: env.NEXT_PUBLIC_APP_URL,
+  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
+  links: {
+    github: 'https://github.com/devchaudhary24k/UrbanRhetoric',
+    twitter: 'https://twitter.com/dev_talan',
+  },
+};
