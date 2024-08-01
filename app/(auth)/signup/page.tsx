a22f486aaab6b4b18927aa5925dd87514eec9e33
Dev Talan <chaudhary.dev.talan@gmail.com>
2024-08-01 13:52:56 +0530

    feat: added signup pade boilerplate

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,14 @@
+// Global Imports
+
+// Internal Imports
+
+// Types
+// type SignUpProps = {};
+
+// Component
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
+
+// Exports
+export default SignUp;
